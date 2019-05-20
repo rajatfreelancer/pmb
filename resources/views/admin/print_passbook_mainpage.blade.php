1842e744635d73760a65b36b19d13113a4f9c765
rajat <rajatfreelancer019@gmail.com>
2019-05-20 10:11:01 +0530

    work on passbook fix

--- resources/views/admin/print_passbook_mainpage.blade.php
+++ resources/views/admin/print_passbook_mainpage.blade.php
@@ -25,7 +25,7 @@
 </head>
 <body class="print_setting">
 
-<div class="container" style="line-height: 1.1; margin: 304px 5px 5px 5px;height: 304px;width: 100%;">
+<div class="container" style="line-height: 1.5; margin: 315px 5px 5px 5px;height: 304px;width: 100%;">
     <div class="row">
         <div>
             <p> <a id="print" class="btn btn-primary ui button blue">Print</a>
